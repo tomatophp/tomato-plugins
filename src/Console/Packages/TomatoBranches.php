7bf87042e6070fd847fc0d98301864a47bdcc5bb
Fady Mondy <info@3x1.io>
2024-01-21 17:06:06 +0200

    remove create default branches from blueprint

--- src/Console/Packages/TomatoBranches.php
+++ src/Console/Packages/TomatoBranches.php
@@ -21,34 +21,5 @@ class TomatoBranches extends TomatoPackage
 
     public function custom(): void
     {
-        $checkIfCompanyExists = Company::count();
-        if ($checkIfCompanyExists < 1) {
-            $company = Company::create([
-                'country_id' => Country::first()?->id,
-                'name' => "main company",
-                'ceo' => "CEO",
-                'address' => "Cairo, Egypt",
-                'city' => "Cairo",
-                'zip' => "110821",
-                'email' => "info@3x1.io",
-                'phone' => "+201207860084",
-                'website' => "https://docs.tomatophp.com"
-            ]);
-        } else {
-            $company = Company::first();
-        }
-
-
-        $checkIfBranchExists = Branch::count();
-        if ($checkIfBranchExists < 1) {
-            $branch = Branch::create([
-                "name" => "main branch",
-                'company_id' => $company->id,
-                'branch_number' => "001",
-                'phone' => "+201207860084",
-                'address' => "Cairo, Egypt"
-            ]);
-        }
-
     }
 }
